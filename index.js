b191301c517a593d60a9116337a58e8e434fd7c9
khetanvallurupalli <32341967+khetanvallurupalli@users.noreply.github.com>
2019-07-16 13:03:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 const { DATABASE_URL } = process.env;
 const server = http.createServer((req, res) => {
   const client = new Client({
-    connectionString: DATABASE_URL,
+    connectionString: "postgres://eazwukoicfoaif:5bdd452dbdf855a81f77d1c225300421e22c6ea9d87adc73c2deae759308b52d@ec2-54-225-242-183.compute-1.amazonaws.com:5432/d2rkbvpbbgm7mu",
   });
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
